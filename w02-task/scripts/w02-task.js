3c22dce69002f1547f6e0ee83a8c82d44720b641
Danicd7 <danielsilva_88@outlook.com>
2023-09-21 19:44:14 +0200

    commit

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -1,10 +1,11 @@
 /* W02-Task - Profile Home Page */
 
+
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+const fullName = "Daniel Candido Caetano Da Silva";
+const year = date.getFullYear();
 
 
 /* Step 3 - Element Variables */
